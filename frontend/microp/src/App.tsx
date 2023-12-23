e6202c50ca157915af7672aec20883c4a2515879
smartfellah <smartfellah@outlook.com>
2023-12-23 19:04:31 +0300

    feat: :sparkles: Add app header
    Also added react-router and react-router-dom dependency and implemented simple routing

--- frontend/microp/src/App.tsx
+++ frontend/microp/src/App.tsx
@@ -1,7 +1,21 @@
 import "./App.css";
 
+//router imports
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+//components imports
+import { Header } from "./components/app-header";
+import { MainPage } from "./pages/main-page";
+
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
