099d7cfa0c9477881d7829ee0838b0ef36ba735e
joanni46 <evgenijserdukov58@gmail.com>
2023-12-23 16:33:03 +0300

    wip

---
+++ frontend/first/src/js/index.js
@@ -0,0 +1,6 @@
+import mobileNav from './modules/mobile-nav.js';
+var script = document.createElement('script');
+script.src = './modules/auth.js';
+document.head.appendChild(script);
+mobileNav();
+
